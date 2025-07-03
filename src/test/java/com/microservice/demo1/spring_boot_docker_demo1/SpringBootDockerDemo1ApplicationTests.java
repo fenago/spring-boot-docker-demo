8c0faf526d5fe1462dff4c8f2c49964c848855ce
Dr. Ernesto Lee <socrates73@gmail.com>
2025-07-03 10:51:01 -0400

    Add files via upload

---
+++ src/test/java/com/microservice/demo1/spring_boot_docker_demo1/SpringBootDockerDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.demo1.spring_boot_docker_demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
